4f74434c548bbfc2f20c6c993bb94c7909fac9a4
Brian Tallam <tallambrian633@gmail.com>
2025-06-26 18:56:26 +0300

    testing now

--- backend/config.py
+++ backend/config.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'a_very_secret_key_that_should_be_changed'
-    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'super-secret-jwt-key'
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'bab4bdd235864a4eaed6c1a64794d3fe'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
                               'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
